e0c100bf8b3630cb47019a1b53b42f3ed0069bd7
Дмитрий Малофеев <dim2k2006@yandex.ru>
2020-07-02 22:22:04 +0300

    Add sqrt

---
+++ sicp/chapter1/sqrt.rkt
@@ -0,0 +1,28 @@
+#lang racket
+
+(require rackunit)
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(check-equal? (sqrt 9) 3.00009155413138)
+(check-equal? (sqrt (+ 100 37)) 11.704699917758145)
+(check-equal? (sqrt (+ (sqrt 2) (sqrt 3))) 1.7739279023207892)
+(check-equal? (square (sqrt 1000)) 1000.000369924366)
